8f3757f409130439d938723c73eb72b1292e16f3
nyinyi76854 <148785112+nyinyi76854@users.noreply.github.com>
2025-02-28 21:08:36 +0630

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,34 @@ self.addEventListener('install', (event) => {
     caches.open('my-cache').then((cache) => {
       return cache.addAll([
         '/', 
+        '/FalconS.ico',
+        '/Toyota.png',
+        '/about_falcons.html',
+        '/chat_layout.html',
+        '/convo.png',
+        '/falcons.png',
+        '/genix.png',
+        '/google46300a22fe6d73e3.html',
+        '/group.jpg',
+        '/group_chat_layout.html',
+        '/host.html',
+        '/hostchat.html',
+        '/hostsearchandstart.html',
         '/index.html',
-        
+        '/interact.js',
+        '/maininterface.html',
+        '/participantview.html',
+        '/post_social.html',
+        '/profile.png',
+        '/profile_of_user.html',
+        '/robots.txt',
+        '/search.html',
+        '/signin.html',
+        '/sitemap.xml',
+        '/upload_group_profile.html',
+        '/user_1077114.png',
+        '/verification.png',
+        '/view_members.html',
         
       ]);
     })
